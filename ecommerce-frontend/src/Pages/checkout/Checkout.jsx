cc4ed064601c81431d36e3ac159171224820a6e5
Thirumalaivasan-Nagarajan <thirumalaivasann.dev@gmail.com>
2026-03-10 09:12:00 +0530

    Creating an cleaner code

--- ecommerce-frontend/src/Pages/checkout/Checkout.jsx
+++ ecommerce-frontend/src/Pages/checkout/Checkout.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 import { OrderSummary } from './OrderSummary';
+import { PaymentSummary } from './PaymentSummary';
 
 export function Checkout({ cart }) {
 
@@ -52,52 +53,7 @@ export function Checkout({ cart }) {
         <div className="checkout-grid">
           <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
 
-          <div className="payment-summary">
-            <div className="payment-summary-title">
-              Payment Summary
-            </div>
-
-            {paymentSummary && (
-
-              <>
-
-              <div className="payment-summary-row">
-              <div>
-              Items ({paymentSummary.totalItems}):
-              </div>
-              <div className="payment-summary-money">${(paymentSummary.productCostCents/100).toFixed(2)}</div>
-            </div>
-
-            <div className="payment-summary-row">
-              <div>Shipping &amp; handling:</div>
-              <div className="payment-summary-money">${(paymentSummary.shippingCostCents).toFixed(2)}</div>
-            </div>
-
-            <div className="payment-summary-row subtotal-row">
-              <div>Total before tax:</div>
-              <div className="payment-summary-money">${(paymentSummary.totalCostBeforeTaxCents).toFixed(2)}</div>
-            </div>
-
-            <div className="payment-summary-row">
-              <div>Estimated tax (10%):</div>
-              <div className="payment-summary-money">${(paymentSummary.taxCents/100).toFixed(2)}</div>
-            </div>
-
-            <div className="payment-summary-row total-row">
-              <div>Order total:</div>
-              <div className="payment-summary-money">${(paymentSummary.totalCostCents/100).toFixed(2)}</div>
-            </div>
-
-            <button className="place-order-button button-primary">
-              Place your order
-            </button>
-
-              </>
-            )
-            }
-
-
-        </div>
+          < PaymentSummary paymentSummary={paymentSummary} />
       </div>
     </div >
 
